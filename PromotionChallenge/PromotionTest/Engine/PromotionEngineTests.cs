06cf238826416ce44e2d0c028a1a216991a5ad0f
Daniel Frederiksen <df@alpha-solutions.dk>
2021-06-24 07:37:48 +0200

    Test setup of scenario B

--- PromotionChallenge/PromotionTest/Engine/PromotionEngineTests.cs
+++ PromotionChallenge/PromotionTest/Engine/PromotionEngineTests.cs
@@ -6,16 +6,20 @@ namespace Promotion.Tests.Engine
 {
     public class PromotionEngineTests
     {
+        Product A = new Product() { Price = 50, Sku = "A" };
+        Product B = new Product() { Price = 30, Sku = "B" };
+        Product C = new Product() { Price = 20, Sku = "C" };
+        private readonly PriceEvaluator _priceEvaluator;
+
+        PromotionEngineTests(PriceEvaluator priceEvaluator)
+        {
+            _priceEvaluator = priceEvaluator;
+        }
+
         [Fact]
         public void TestScenarioA()
         {
             //Arrange
-            var priceEvaluator = new PriceEvaluator();
-
-            Product A = new Product() { Price = 50, Sku = "A" };
-            Product B = new Product() { Price = 30, Sku = "B" };
-            Product C = new Product() { Price = 20, Sku = "C" };
-            
             BasketLine basketLine1 = new BasketLine() { BasketLineProduct = A, Quantity = 1 };
             BasketLine basketLine2 = new BasketLine() { BasketLineProduct = B, Quantity = 1 };
             BasketLine basketLine3 = new BasketLine() { BasketLineProduct = C, Quantity = 1 };
@@ -26,10 +30,33 @@ namespace Promotion.Tests.Engine
             basket.BasketLines.Add(basketLine3);
 
             //Act
-            var result = priceEvaluator.EvaluatePrice(basket);
+            var result = _priceEvaluator.EvaluatePrice(basket);
 
             //Assert
             Assert.Equal(result, A.Price + B.Price + C.Price);
         }
+
+        [Fact]
+        public void TestScenarioB()
+        {
+            //Arrange
+            BasketLine basketLine1 = new BasketLine() { BasketLineProduct = A, Quantity = 5 };
+            BasketLine basketLine2 = new BasketLine() { BasketLineProduct = B, Quantity = 5 };
+            BasketLine basketLine3 = new BasketLine() { BasketLineProduct = C, Quantity = 1 };
+
+            Basket basket = new Basket();
+            basket.BasketLines.Add(basketLine1);
+            basket.BasketLines.Add(basketLine2);
+            basket.BasketLines.Add(basketLine3);
+
+            //Act
+            var result = _priceEvaluator.EvaluatePrice(basket);
+
+            //Assert
+            Assert.Equal(370, result);
+        }
+
+
+
     }
 }
